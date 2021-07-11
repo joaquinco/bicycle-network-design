3711186b4dbb43c65f8cd503f8eea4a00c907a7d
Joaquin <joaquincorrea333@gmail.com>
2021-07-11 09:48:29 -0300

    add solution characteristics

---
+++ bcnetwork/validation.py
@@ -0,0 +1,11 @@
+def validate_solution(model, solution):
+    """
+    Performs the validations specified in the document:
+    1. El costo de los caminos entre pares origen-destino sobre la red resultante
+    es menor o igual al costo sobre la red sin infraestructuras.
+    2. El presupuesto excedente no es suficiente para agregar una infraestructura
+    que mejore el costo de alguno de los caminos.
+    3. El camino más corto sobre la red resultante para un par origen-destino no
+    puede resultar en un valor de demanda transferida distinto al resultante.
+    """
+    pass
