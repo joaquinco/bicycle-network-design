4afe4c3102c09e1dd001e058fdb351880d2d1f75
Joaquin <joaquincorrea333@gmail.com>
2021-10-04 17:01:18 -0300

    refactor drawing methods

--- scripts/processruns.py
+++ scripts/processruns.py
@@ -97,6 +97,7 @@ def format_run_time_label(x, pos):
             return f'{hours}h'
         return f'{hours}h {minutes}m'
 
+
 model_colormap = {
     "default": bc.colors.blue,
     "single_level_v2": bc.colors.orange,
@@ -106,27 +107,27 @@ model_colormap = {
     "single_level_v6": bc.colors.green,
 }
 
+
 def draw_time_comparison(run_data, output_prefix):
     """
     Draw running time comparison between instances.
     """
-    plot_last_count = 200
+    # Plot last quintile.
+    plot_last_count = len(run_data.df) // len(run_data.model_names) // 5
     half_hour_seconds = 1800
     hour_seconds = half_hour_seconds * 2
     draw_best_count = 2
 
     fig, (ax, ax2) = plt.subplots(2, 1)
 
-    model_names = set(run_data.df.model_name)
-
     plot_axis = list(range(len(run_data.df)))
 
-    ax.plot(plot_axis[-plot_last_count:], [run_data.max_run_time_limit] * plot_last_count, '--', color='#ccc', linewidth=1)
+    bests_models = set(
+        run_data.avg_by_model_name_df.model_name.iloc[:draw_best_count])
 
-    bests_models = set(run_data.avg_by_model_name_df.model_name.iloc[:draw_best_count])
-
-    for model_name in sorted(model_names):
-        df_by_model = run_data.sort_by_avg_run_time(run_data.df[run_data.df.model_name == model_name])
+    for model_name in run_data.model_names:
+        df_by_model = run_data.sort_by_avg_run_time(
+            run_data.df[run_data.df.model_name == model_name])
         plot_args = (
             plot_axis[-plot_last_count:],
             df_by_model.run_time_seconds.iloc[-plot_last_count:],
@@ -141,9 +142,9 @@ def draw_time_comparison(run_data, output_prefix):
         if model_name in bests_models:
             ax2.plot(*plot_args, **plot_kwargs)
 
-    ax.set_yticks([hour_seconds * i for i in range(int(run_data.max_run_time_limit) // hour_seconds + 1)])
     # Hide x labels because they make no sense
     ax.set_title('Run time comparison')
+    ax.set_yscale('log')
 
     for curr_ax in [ax, ax2]:
         curr_ax.tick_params(axis='y', which='both', labelrotation=45)
@@ -152,15 +153,64 @@ def draw_time_comparison(run_data, output_prefix):
         curr_ax.xaxis.set_major_formatter(lambda x, pos: '')
         curr_ax.legend()
 
-    ax2.set_xlabel(f'Instances sorted by avg. run time - last {plot_last_count}')
+    ax2.set_xlabel(f'Instances sorted by avg. run time - last quintile')
     fig.savefig(f'{output_prefix}run_time_comparison.png', **savefig_kwargs)
 
 
-def draw_quintil_time_comparison(df, output_prefix):
+def draw_quintile_time_comparison(run_data, output_prefix):
     """
     Plot an histogram of each mean run time for each quintile of the instances.
     """
-    
+    quintile_size = len(run_data.df) // len(run_data.model_names) // 5
+
+    fig, ax = plt.subplots()
+    sorted_models = {
+        model_name: run_data.sort_by_avg_run_time(
+            run_data.df[run_data.df.model_name == model_name])
+        for model_name in run_data.model_names
+    }
+    bar_width = 0.15
+    model_count = len(run_data.model_names)
+
+    data = {}
+    # Plot the first 4 quintiles
+    quintiles = np.array(range(1, 5))
+
+    for model_name in run_data.model_names:
+        model_means = []
+        for quintile in quintiles:
+            df_slice = sorted_models[model_name].iloc[0:quintile * quintile_size]
+
+            model_means.append(df_slice.run_time_seconds.mean())
+
+        data[model_name] = model_means
+
+    for index, value in enumerate(data.items()):
+        model_name, mean_values = value
+
+        x = quintiles - bar_width * (model_count / 2 - index)
+
+        ax.bar(
+            x,
+            mean_values,
+            bar_width,
+            color=model_colormap[model_name],
+            align='center',
+            label=get_model_version(model_name)
+        )
+
+    ax.yaxis.set_major_formatter(format_run_time_label)
+    # ax.set_yscale('log')
+    ax.set_xticks(quintiles)
+    ax.set_xticklabels([f'Q{idx}' for idx in quintiles])
+    ax.legend()
+
+    ax.set_title('Run time comparison per quintile')
+    ax.set_xlabel('Quintile of instances sorted by avg. run time')
+    ax.set_ylabel('Avg. run time')
+
+    fig.savefig(f'{output_prefix}_mean_run_time.png', **savefig_kwargs)
+
 
 class InstanceDrawData:
     def __init__(self, df):
@@ -176,13 +226,20 @@ class InstanceDrawData:
 
     @cached_property
     def avg_by_model_name_df(self):
-       return self.df.groupby('model_name', as_index=False)['run_time_seconds'].mean().sort_values(by=['run_time_seconds'])
+        return self.df.groupby('model_name', as_index=False)['run_time_seconds'].mean().sort_values(by=['run_time_seconds'])
+
+    @cached_property
+    def model_names(self):
+        model_names = set(self.df.model_name)
+
+        return list(sorted(model_names))
 
     @cached_property
     def max_run_time_limit(self):
         # Limit max run times to some value so
         # plot is better visible
-        max_run_time = self.mean_run_time_df.run_time_seconds.std() + self.max_run_time_mul * self.mean_run_time_df.run_time_seconds.std()
+        max_run_time = self.mean_run_time_df.run_time_seconds.std(
+        ) + self.max_run_time_mul * self.mean_run_time_df.run_time_seconds.std()
         # Align to half an hour precision
         max_run_time = max_run_time + max_run_time % 1800
 
@@ -192,20 +249,20 @@ class InstanceDrawData:
     def avg_run_time_indexer(self):
         return dict(zip(self.mean_run_time_df.model, range(len(self.mean_run_time_df))))
 
-    def sort_by_avg_run_time(self, other_df, set_limit=True):
+    def sort_by_avg_run_time(self, other_df):
         other_df['index'] = other_df.model.map(self.avg_run_time_indexer)
         other_df.sort_values(by=['index', 'model'], inplace=True)
-        if set_limit:
-            other_df.loc[other_df.run_time_seconds > self.max_run_time_limit, 'run_time_seconds'] = self.max_run_time_limit
 
         return other_df
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('-m', '--model-count', help='Number of different model versions', type=int, default=4)
+    parser.add_argument('-m', '--model-count',
+                        help='Number of different model versions', type=int, default=4)
     parser.add_argument('-o', '--output', help='Output directory path')
-    parser.add_argument('-a', '--actions', help='Actions to perform', nargs='+', default='pre', choices=['pre', 'post'])
+    parser.add_argument('-a', '--actions', help='Actions to perform',
+                        nargs='+', default='pre', choices=['pre', 'post'])
     parser.add_argument('runs_path', help='Path of the runs file')
 
     args = parser.parse_args(sys.argv[1:])
@@ -234,9 +291,11 @@ def main():
         save_df(completed_df, 'completed')
 
         extractors = [
-            ('differences', partial(extract_runs_with_differences, completed_df, args.model_count)),
+            ('differences', partial(extract_runs_with_differences,
+             completed_df, args.model_count)),
             ('errors', partial(extract_runs_with_errors, df)),
-            ('errors_over_completed', partial(extract_runs_with_errors, completed_df)),
+            ('errors_over_completed', partial(
+                extract_runs_with_errors, completed_df)),
             ('comparison', partial(extract_model_comparison, df)),
             ('comparison_over_completed', partial(
                 extract_model_comparison, completed_df)),
@@ -249,8 +308,8 @@ def main():
     if 'post' in args.actions:
         run_data = InstanceDrawData(completed_df)
         draw_time_comparison(run_data, output_file_prefix)
-        draw_quintil_time_comparison(run_data, output_file_prefix)
+        draw_quintile_time_comparison(run_data, output_file_prefix)
 
 
 if __name__ == '__main__':
-   main()
+    main()
