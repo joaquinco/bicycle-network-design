340278ce9e74de643288a346206c5bfa7aae8e35
Joaquin <joaquincorrea333@gmail.com>
2021-10-06 19:42:01 -0300

    add missing files

---
+++ bcnetwork/colors.py
@@ -0,0 +1,11 @@
+from .bunch import Bunch
+
+
+colors = Bunch({
+    'gray_light': '#b3b3b3',
+    'gray_dark': '#303030',
+    'blue': '#3b6ca7',
+    'orange': '#f14902',
+    'yellow': '#f5b028',
+    'green': '#417f55',
+})
