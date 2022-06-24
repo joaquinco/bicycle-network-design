80d09f3c9baaf90525aa7dc81cbaaf3290097510
Joaquin <joaquincorrea333@gmail.com>
2022-06-24 19:46:17 -0300

    add comment on infra construction cost

--- instances/generate_montevideo_instances.sh
+++ instances/generate_montevideo_instances.sh
@@ -12,33 +12,40 @@ fi
 mkdir -p $output_dir
 
 generate_instances() {
+    # python scripts/runmontevideo.py \
+    #     --function $1 \
+    #     --name-suffix $1 \
+    #     --breakpoint-count $breakpoint_count \
+    #     --max-distance $max_distance $output_dir
+    # python scripts/runmontevideo.py \
+    #     --function $1 \
+    #     --name-suffix $1_0.4_budget_factor \
+    #     --breakpoint-count $breakpoint_count \
+    #     --max-distance $max_distance $output_dir \
+    #     --budget-factor 0.4
+    # python scripts/runmontevideo.py \
+    #     --function $1 \
+    #     --name-suffix $1_1.6_budget_factor \
+    #     --breakpoint-count $breakpoint_count \
+    #     --max-distance $max_distance $output_dir \
+    #     --budget-factor 1.6
+    # python scripts/runmontevideo.py \
+    #     --function $1 \
+    #     --name-suffix $1_0.04_budget_factor \
+    #     --breakpoint-count $breakpoint_count \
+    #     --max-distance $max_distance $output_dir \
+    #     --budget-factor 0.04
     python scripts/runmontevideo.py \
         --function $1 \
-        --name-suffix $1 \
-        --breakpoint-count $breakpoint_count \
-        --max-distance $max_distance $output_dir
-    python scripts/runmontevideo.py \
-        --function $1 \
-        --name-suffix $1_0.4_budget_factor \
+        --name-suffix $1_0.06_budget_factor \
         --breakpoint-count $breakpoint_count \
         --max-distance $max_distance $output_dir \
-        --budget-factor 0.4
-    python scripts/runmontevideo.py \
-        --function $1 \
-        --name-suffix $1_1.6_budget_factor \
-        --breakpoint-count $breakpoint_count \
-        --max-distance $max_distance $output_dir \
-        --budget-factor 1.6
-    python scripts/runmontevideo.py \
-        --function $1 \
-        --name-suffix $1_0.04_budget_factor \
-        --breakpoint-count $breakpoint_count \
-        --max-distance $max_distance $output_dir \
-        --budget-factor 0.04
+        --budget-factor 0.06
 }
 
 # Equivalencia con el presupuesto asignado en Montevideo.
 # (0.40 + 2 * 0.63 + 4 * 0.24) / 100 = 0.026
+# Dicho valor multiplicado por dos porque ahi se construye ida y vuelta.
 
 generate_instances linear
 generate_instances inv_logit
