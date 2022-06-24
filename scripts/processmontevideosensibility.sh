daf668bd08fa0298f4e66446dc609ab461db5e98
Joaquin <joaquincorrea333@gmail.com>
2022-06-24 19:57:08 -0300

    add montevideo sensibility script caller

---
+++ scripts/processmontevideosensibility.sh
@@ -0,0 +1,9 @@
+python scripts/processsensitivity.py \
+    --demand-by-budget-breakpoint-count 10 \
+    --draw-width 0.3 \
+    --draw-skip-flows \
+    --draw-skip-odpairs \
+    --draw-fig-width 8 \
+    --draw-fig-height 7 \
+    --draw-node-size 0.1 \
+    --draw-skip-labels $@
